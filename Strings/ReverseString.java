46fd13b72e043082ec4e22c034ae52b779fd30d2
yaswanth24k <saiyaswanth24k@gmail.com>
2025-12-26 16:28:35 +0530

    Solutions for String problems on Leetcode

---
+++ Strings/ReverseString.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void reverseString(char[] s) {
+        reverse(s,0,s.length-1);
+    }
+    public void reverse(char[] s,int left,int right)
+    {
+        if(left>right)
+        {
+            return;
+        }
+        char temp=s[left];
+        s[left]=s[right];
+        s[right]=temp;
+
+        reverse(s,left+1,right-1);
+
+    }
+}
