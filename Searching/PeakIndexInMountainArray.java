c0d307519e586fe785d461c4be050fe9183a1cf2
yaswanth24k <saiyaswanth24k@gmail.com>
2025-12-28 15:37:00 +0530

    Solutions for Searching on Leetcode

---
+++ Searching/PeakIndexInMountainArray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1])
+            {
+                return mid;
+            }
+            else if(arr[mid]<arr[mid+1])
+            {
+                start=mid;
+            }
+            else
+            {
+                end=mid;
+            }
+        }
+        return -1;
+    }
+}
