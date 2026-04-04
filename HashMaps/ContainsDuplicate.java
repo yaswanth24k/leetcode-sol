06c282e53bf29945b126d708e8bc9fb6ae969d9a
yaswanth24k <saiyaswanth24k@gmail.com>
2026-04-04 22:16:21 +0530

    Solution to Contains Duplicate 1 2 and 2 sum is added

---
+++ HashMaps/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       HashSet<Integer> set=new HashSet<>();
+       for(int num :nums)
+       {
+        if(set.contains(num))
+        {
+            return true;
+        }
+        set.add(num);
+       } 
+       return false;
+    }
+}
