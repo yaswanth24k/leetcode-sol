7507b73532a026256cc89bfbeeb4a5ef0e6e4c00
yaswanth24k <saiyaswanth24k@gmail.com>
2026-04-18 19:31:22 +0530

    solution to container with most water is added

---
+++ TwoPointers/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+import java.math.*;
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0;
+        int left=0;
+        int right=height.length-1;
+        while(left<right)
+        {
+            int area=Math.min(height[left],height[right])*(right-left);
+            max=Math.max(area,max);
+            if(height[left]<height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+            
+        }
+        return max;
+    }
+}
