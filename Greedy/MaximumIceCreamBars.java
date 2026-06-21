d227e2fc6b52cd2f091a07453bd9eab219efc37f
yaswanth24k <saiyaswanth24k@gmail.com>
2026-06-21 22:05:26 +0530

    Solution to maximum ice cream bars is added

---
+++ Greedy/MaximumIceCreamBars.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count=0;
+        for(int i:costs)
+        {
+            if(coins>=i)
+            {
+                coins-=i;
+                count++;
+            }
+        }
+        return count;
+    }
+}
