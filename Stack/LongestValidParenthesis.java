673e3070e5b006cc2636832dcaa5e29ee73335a7
yaswanth24k <saiyaswanth24k@gmail.com>
2026-06-25 21:43:20 +0530

    solution to longestValidParentheses is added

---
+++ Stack/LongestValidParenthesis.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack=new Stack<Integer>();
+        int max=0;
+        stack.push(-1);
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                stack.push(i);
+            }
+            else
+            {
+                stack.pop();
+                if(stack.isEmpty())
+                {
+                    stack.push(i);
+                }
+                else
+                {
+                    max=Math.max(max,i-stack.peek());
+                }
+            }
+        }
+        return max;
+    }
+}
