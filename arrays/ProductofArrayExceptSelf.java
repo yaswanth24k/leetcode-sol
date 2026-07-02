c96dbf07453de4552c1b2af18a639155315b2ba0
yaswanth24k <saiyaswanth24k@gmail.com>
2026-07-02 23:20:30 +0530

    solution toproduct of array except self

---
+++ arrays/ProductofArrayExceptSelf.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res =new int[nums.length];
+        res[0]=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            res[i]=res[i-1]*nums[i-1];
+        }
+        int suf=1;
+        for(int j=nums.length-1;j>=0;j--)
+        {
+            res[j]*=suf;
+            suf*=nums[j];
+        }
+        return res;
+    }
+}
