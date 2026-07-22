dfcc39dba9bb77a19041b985fcda0e363fadce52
yaswanth24k <saiyaswanth24k@gmail.com>
2026-07-22 14:35:31 +0530

    Refactor InvertTree method to invert binary tree

--- Trees/InvertBinaryTree.java
+++ Trees/InvertBinaryTree.java
@@ -1,28 +1,29 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
  *     }
  * }
  */
-public class Solution {
-    public TreeNode InvertTree(TreeNode root) {
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
         if(root==null)
         {
             return null;
         }
-        InvertTree(root.left);
-        InvertTree(root.right);
-        TreeNode temp=root.left;
-        root.left=root.right;
-        root.right=temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp=root.right;
+        root.right=root.left;
+        root.left=temp;
         return root;
-
     }
 }
