e4f2c8ecefc4406f011797c9639618d81221ba94
yaswanth24k <saiyaswanth24k@gmail.com>
2026-08-03 23:04:18 +0530

    solution to maximum product of three numbers is added

---
+++ arrays/MaximumProductofThreeNumbers.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int pro1=nums[n-1]*nums[n-2]*nums[n-3];
+        int pro2=nums[0]*nums[n-1]*nums[1];
+        return Math.max(pro1,pro2);
+    }
+}
